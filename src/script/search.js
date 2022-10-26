4527454092d32e6da4c726ea1524781ea2097a93
ewerton11 <ewertonrc01@gmail.com>
2022-10-26 13:51:24 -0300

    dados progresso de campeonatos

--- src/script/search.js
+++ src/script/search.js
@@ -48,13 +48,13 @@ async function liberatorsRound() {
     let dataObtained = document.querySelector(".navegacao-fase__fase").innerText
 
     if (dataObtained == "FINAL") {
-      dataObtained = 4
+      dataObtained = 9
     } else {
       // Por semi e quartas e tals
       console.log("nao foi")
     }
 
-    const phase = [dataObtained, 5 - dataObtained]
+    const phase = [dataObtained, 10 - dataObtained]
 
     return phase
   })
@@ -69,3 +69,41 @@ async function liberatorsRound() {
 }
 
 liberatorsRound()
+
+async function brazilCup() {
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox"],
+    headless: true,
+  })
+
+  const page = await browser.newPage()
+
+  page.setDefaultNavigationTimeout(0)
+
+  await page.goto("https://ge.globo.com/futebol/copa-do-brasil/")
+
+  const phaseRound = await page.evaluate(() => {
+    let dataObtained = document.querySelector(".navegacao-fase__fase").innerText
+
+    if (dataObtained == "FINAL") {
+      dataObtained = 9
+    } else {
+      // Por semi e quartas e tals
+      console.log("nao foi")
+    }
+
+    const phase = [dataObtained, 10 - dataObtained]
+
+    return phase
+  })
+
+  const data = fs.readFileSync("chartInformation.json")
+  const converting = JSON.parse(data)
+  converting.brazilCup = phaseRound
+
+  fs.writeFileSync("chartInformation.json", JSON.stringify(converting, null, 2))
+
+  await browser.close()
+}
+
+brazilCup()
